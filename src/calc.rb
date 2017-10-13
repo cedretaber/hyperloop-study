019cbf42375b8c2cfcb10bec368c14abeb776dc6
cedretaber <cedretaber@gmail.com>
2017-10-14 04:07:07 +0900

    Refactor.

--- src/calc.rb
+++ src/calc.rb
@@ -4,16 +4,16 @@ class CalcComponent
   state x: 0
   state y: 0
 
-  render(DIV) do
-    make_input :x, state.x.to_s
+  render DIV do
+    CalcInput key: :x, value: state.x.to_s
     SPAN { "+" }
-    make_input :y, state.y.to_s
+    CalcInput key: :y, value: state.y.to_s
     SPAN { "=#{state.x + state.y}" }
   end
 
   private
 
-  def make_input(key, value)
+  def CalcInput(key:, value:)
     INPUT(value: value).on(:change) { |e| mutate.__send__(key, (e.target.value.to_i || 0)) }
   end
 end
